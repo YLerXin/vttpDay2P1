434cf4896a7ccb08ed0dc500b2d137c7db58872d
Yong_Ler_Xin <yonglerxin@gmail.com>
2024-10-04 09:15:26 +0800

    Updated: added more lines

--- src/project/DeckT.java
+++ src/project/DeckT.java
@@ -1,4 +1,6 @@
 package project;
+//DeckT more closely align to what the professor does whereas Deck is the one I wrote
+import java.util.Random;
 
 public class DeckT {
 
@@ -26,6 +28,18 @@ public class DeckT {
         }
 
     }
+//DeckT deck = new DeckT();
+    public Card[] shuffle(){
+
+        Random rand = new Random();
+        for (int i = 0; i < cards.length; i++) {
+                int randomIndexToSwap = rand.nextInt(52); // choose a rand num from 0 to 51 i guess
+                Card temp = cards[randomIndexToSwap];
+                cards[randomIndexToSwap] = cards[i];
+                cards[i] = temp;
+            }
+        return cards;
+    }
 
     public Card get(){
         return this.cards[0];
@@ -33,7 +47,9 @@ public class DeckT {
     public Card get(int pos){
         return this.cards[pos];
     }
-
+    public Card[] getDeck() {
+        return this.cards;  // Returns the deck (the array of cards)
+    }
     @Override
     public String toString(){
         return "Number of cards: %d".formatted(cards.length);
--- src/project/Main.java
+++ src/project/Main.java
@@ -2,11 +2,24 @@ package project;
 
 public class Main {
 
+    //suffle the deck
+
     public static void main(String[] args){
         DeckT deck = new DeckT();
         Card card = deck.get(5);
         System.out.printf("Suit: %s, Name: %s, Values: %d,",card.getSuit(),card.getName(),card.getValue());
         System.out.println(card);
+
+        //print deck
+        // for (int i=0; i<52;i++){
+        //     System.out.println(deck.get(i));
+        // }
+        deck.shuffle();
+        Card[] shuffledDeck = deck.getDeck();  // Get the shuffled deck
+
+        for (int i = 0; i < shuffledDeck.length; i++) {
+            System.out.println(shuffledDeck[i]);  // Assuming 'Card' has a proper toString() method to display the card
+        }
     }
     
 }
